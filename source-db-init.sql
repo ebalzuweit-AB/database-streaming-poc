cafb8873754a49329c857056443fc8d9cd6435b1
Evan Balzuweit <evan.balzuweit@anheuser-busch.com>
2024-12-19 10:35:27 -0500

    Add multiple tables & sink connectors

--- source-db-init.sql
+++ source-db-init.sql
@@ -1,7 +1,24 @@
 -- Setup database
 CREATE SCHEMA streaming_poc;
-CREATE TABLE streaming_poc.test(
-	id int primary key
+
+CREATE TABLE streaming_poc.users (
+	id serial primary key,
+	name varchar(100) not null,
+	created_at timestamptz default current_timestamp
+);
+
+CREATE TABLE streaming_poc.orders (
+	id serial primary key,
+	user_id int not null,
+	created_at timestamptz default current_timestamp,
+	updated_at timestamptz default current_timestamp
+);
+
+CREATE TABLE streaming_poc.order_items (
+	id serial primary key,
+	order_id int not null,
+	product varchar(100) not null,
+	quantity int not null
 );
 
 -- Required for Debezium
